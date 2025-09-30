3fa1c84c02cf6f26c47c0dd67bccaf5a8d27e096
FernandoEspinoza07 <fernandoespinozasantos@gmail.com>
2025-09-29 23:46:44 -0600

    Conflicto resuelto

--- ejercicio8.py
+++ ejercicio8.py
@@ -1,8 +1,4 @@
-conflicto
+
 print("Versión rama conflicto")
-
-// Versión final después de resolver
-console.log(print("Versión rama main"));
-
-
- main
+print("Versión rama main")
+print("conflicto resuelto")
